37ec18dac245bb6ceac627e2ca4a5ac6af893101
Nadezhda <nbykova79@gmail.com>
2022-07-23 09:18:15 +0300

    save task 64

---
+++ TASK64/Program.cs
@@ -0,0 +1,10 @@
+﻿// По заданным значения N и M выводит все натуральные числа в промежутке от M до N.
+Console.WriteLine("Введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите M ");
+int m = Convert.ToInt32(Console.ReadLine());
+void ScrinNum (int n, int m)
+{
+    for (int i=n; i<= m; i++) {Console.Write($"{i} ");}
+}
+ScrinNum (n,m);
